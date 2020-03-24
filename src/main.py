2460a22c5993e65172a0af037f228cefb363b7d5
YeonwooSung <neos960518@gmail.com>
2020-03-25 05:35:25 +0900

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: main.py [-binary | -multi] [estimator_name]')
+        exit(1)
+
+    mode = sys.argv[1]
+    estimator_name = sys.argv[2]
+
+    #TODO validate estimator name
+
+    if mode == 'binary':
+        print('Start binary classification')
+    
+        #TODO
+
+    elif mode == 'multi':
+        print('Start multi-class classification')
+    
+        #TODO
+
+    else:
+        print('Usage: main.py [-binary | -multi] [estimator_name]')
+        exit(1)
