f0201a01b30b4677106a17e088d819984ac1b500
YeonwooSung <neos960518@gmail.com>
2020-03-30 17:23:27 +0900

    Implement standardization

--- src/features.py
+++ src/features.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
 
 
 
 def cleanData(df):
+    """
+    Clean data by replace NaN values with median.
+    """
     # use SimpleImputer to clean training data with median values
     imputer = SimpleImputer(strategy='median')
     imputer.fit(df)
@@ -13,19 +17,23 @@ def cleanData(df):
     # convert the numpy array x to dataframe
     df = pd.DataFrame(x, columns=df.columns, index=list(df.index.values))
 
-    #TODO find_columns_by_max_val
+    # standardize columns whose max value is greater than threshold value
+    # use the MinMaxScaler to standardize the target columns
+    df = standardize_big_values_by_min_max(df)
 
     return df
 
 
-def find_columns_by_max_val(df, thresh=100):
+def standardize_big_values_by_min_max(df, thresh=100):
     # find columns whose max value is greater than threshold value
     overThresh = df.max() > thresh
     targetCols = overThresh[overThresh].index.values
 
-    targetCols_df = df[targetCols]
+    scaler = MinMaxScaler()
+    scaler.fit_transform(df[targetCols])
 
-    #TODO standardize
+    return df
+
 
 def mapYValues_binary(df):
     mapping = {'background': 1, 'seal': 2}
