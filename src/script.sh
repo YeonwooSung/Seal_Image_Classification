7153d231133c0f2f3fca4de8ca028d418984f821
YeonwooSung <neos960518@gmail.com>
2020-05-02 21:48:33 +0900

    Update script for evalutation

--- src/script.sh
+++ src/script.sh
@@ -4,21 +4,26 @@ function runMain() {
     mode=$1
     estimator=$2
     fp=$3
+    print_line=$4
 
+    # run python script with given name of estimator
     python3 main.py --mode ${mode} --estimator ${estimator} > ${fp}
+
+    # print finish message
+    echo ${print_line}
 }
 
 
 # binary
-runMain "binary" "logistic" "../binary_logistic.txt"
-runMain "binary" "sgd" "../binary_sgd.txt"
-runMain "binary" "xgb" "../binary_xgb.txt"
-runMain "binary" "rf" "../binary_rf.txt"
-runMain "binary" "vc" "../binary_vc.txt"
+runMain "binary" "logistic" "../binary_logistic.txt" "Finish binary logistic"
+runMain "binary" "sgd" "../binary_sgd.txt" "Finish binary Stochastic Gradient Descent"
+runMain "binary" "xgb" "../binary_xgb.txt" "Finish binary XGBoost"
+runMain "binary" "rf" "../binary_rf.txt" "Finish binary RandomForest"
+runMain "binary" "vc" "../binary_vc.txt" "Finish binary Voting Classification"
 
 # multi
-runMain "multi" "logistic" "../multi_logistic.txt"
-runMain "multi" "sgd" "../multi_sgd.txt"
-runMain "multi" "xgb" "../multi_xgb.txt"
-runMain "multi" "rf" "../multi_rf.txt"
-runMain "multi" "vc" "../multi_vc.txt"
+runMain "multi" "logistic" "../multi_logistic.txt" "Finish multi logistic"
+runMain "multi" "sgd" "../multi_sgd.txt" "Finish multi Stochastic Gradient Descent"
+runMain "multi" "xgb" "../multi_xgb.txt" "Finish multi XGBoost"
+runMain "multi" "rf" "../multi_rf.txt" "Finish multi RandomForest"
+runMain "multi" "vc" "../multi_vc.txt" "Finish multi Voting Classification"
