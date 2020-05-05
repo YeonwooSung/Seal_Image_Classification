f96570719e0b32e36d4ca6ac58567f61ff134708
YeonwooSung <neos960518@gmail.com>
2020-05-05 18:15:33 +0900

    Change filepath of output text files

--- src/script.sh
+++ src/script.sh
@@ -15,15 +15,15 @@ function runMain() {
 
 
 # binary
-runMain "binary" "logistic" "../output/binary_logistic.txt" "Finish binary logistic"
-runMain "binary" "sgd" "../output/binary_sgd.txt" "Finish binary Stochastic Gradient Descent"
-runMain "binary" "rf" "../output/binary_rf.txt" "Finish binary RandomForest"
-runMain "binary" "vc" "../output/binary_vc.txt" "Finish binary Voting Classification"
-runMain "binary" "xgb" "../output/binary_xgb.txt" "Finish binary XGBoost"
+runMain "binary" "logistic" "../result/output/binary_logistic.txt" "Finish binary logistic"
+runMain "binary" "sgd" "../result/output/binary_sgd.txt" "Finish binary Stochastic Gradient Descent"
+runMain "binary" "rf" "../result/output/binary_rf.txt" "Finish binary RandomForest"
+runMain "binary" "vc" "../result/output/binary_vc.txt" "Finish binary Voting Classification"
+runMain "binary" "xgb" "../result/output/binary_xgb.txt" "Finish binary XGBoost"
 
 # multi
-runMain "multi" "logistic" "../output/multi_logistic.txt" "Finish multi logistic"
-runMain "multi" "sgd" "../output/multi_sgd.txt" "Finish multi Stochastic Gradient Descent"
-runMain "multi" "rf" "../output/multi_rf.txt" "Finish multi RandomForest"
-runMain "multi" "vc" "../output/multi_vc.txt" "Finish multi Voting Classification"
-runMain "multi" "xgb" "../output/multi_xgb.txt" "Finish multi XGBoost"
+runMain "multi" "logistic" "../result/output/multi_logistic.txt" "Finish multi logistic"
+runMain "multi" "sgd" "../result/output/multi_sgd.txt" "Finish multi Stochastic Gradient Descent"
+runMain "multi" "rf" "../result/output/multi_rf.txt" "Finish multi RandomForest"
+runMain "multi" "vc" "../result/output/multi_vc.txt" "Finish multi Voting Classification"
+runMain "multi" "xgb" "../result/output/multi_xgb.txt" "Finish multi XGBoost"
